4ff141f1d57d5e117a9cc890a6700ae289c5ea7c
Marcos Defendi <marcos.defendi@rocket.chat>
2025-03-18 17:00:31 -0300

    fix: fix breaking json parse

--- apps/meteor/app/api/server/router.ts
+++ apps/meteor/app/api/server/router.ts
@@ -159,6 +159,12 @@ export class Router<
 					}
 				}
 
+				let bodyParams = {};
+				try {
+					bodyParams = await (req.header('content-type')?.includes('application/json') ? c.req.json() : req.text());
+					// eslint-disable-next-line no-empty
+				} catch {}
+
 				const {
 					body,
 					statusCode = 200,
@@ -167,7 +173,7 @@ export class Router<
 					{
 						urlParams: req.param(),
 						queryParams: req.query(),
-						bodyParams: await (req.header('content-type')?.includes('application/json') ? c.req.json() : req.text()),
+						bodyParams,
 						request: req.raw,
 						response: res,
 					} as any,
