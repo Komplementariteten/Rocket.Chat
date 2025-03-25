5236602258af61c7c1d339ed2905160db38b6f1b
Marcos Defendi <marcos.defendi@rocket.chat>
2025-03-25 13:30:19 -0300

    fix: try to fix import order

--- apps/meteor/ee/server/models/startup.ts
+++ apps/meteor/ee/server/models/startup.ts
@@ -3,11 +3,11 @@ import { License } from '@rocket.chat/license';
 // To facilitate our lives with the stream
 // Collection will be registered on CE too
 // No functionality will be imported tho, just the service registration
-import('./OmnichannelServiceLevelAgreements');
-import('./AuditLog');
-import('./ReadReceipts');
+await import('./OmnichannelServiceLevelAgreements');
+await import('./AuditLog');
+await import('./ReadReceipts');
 
-void License.onLicense('livechat-enterprise', () => {
+await License.onLicense('livechat-enterprise', () => {
 	import('./CannedResponse');
 	import('./LivechatTag');
 	import('./LivechatUnit');
